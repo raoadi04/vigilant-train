e0322c19c9bb70c222a2b7d0c343de689840ff4d
Aditya Rao <44147589+raoadi04@users.noreply.github.com>
2024-07-05 23:34:45 -0400

    Create iterMut.rs
    This mutability allows for the collection to be modified in place

---
+++ iterMut.rs
@@ -0,0 +1,13 @@
+fn main() {
+	let mut names = vec!["Bob", "Frank", "Ferris"];
+
+	for name in names.iter_mut(){
+		*name = match name {
+			&mut "Ferris" => "There is a rustacean among us!",
+			_=> "Hello",
+			//_=> *name,
+		}
+	}
+	
+	println!("names: {:?}", names);
+}
