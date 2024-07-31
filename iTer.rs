ecd835e274fbe0473c0455aa72111c35c0d1704d
Aditya Rao <44147589+raoadi04@users.noreply.github.com>
2024-07-31 19:27:22 -0400

    Create iTer.rs
    Iterator trait using std library

---
+++ iTer.rs
@@ -0,0 +1,12 @@
+pub trait Iterator {
+    // The type being iterated over.
+    type Item;
+
+    // `any` takes `&mut self` meaning the caller may be borrowed
+    // and modified, but not consumed.
+    fn any<F>(&mut self, f: F) -> bool where
+        // `FnMut` meaning any captured variable may at most be
+        // modified, not consumed. `Self::Item` states it takes
+        // arguments to the closure by value.
+        F: FnMut(Self::Item) -> bool;
+}
