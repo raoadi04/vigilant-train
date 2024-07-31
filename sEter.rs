030054d45f6c49efc8bdcd7f26525187b76ee5e9
Aditya Rao <44147589+raoadi04@users.noreply.github.com>
2024-07-31 19:35:50 -0400

    Create sEter.rs
    Using std library, to implement search using iterator trait

---
+++ sEter.rs
@@ -0,0 +1,12 @@
+pub trait Iterator {
+    // The type being iterated over.
+    type Item;
+
+    // `find` takes `&mut self` meaning the caller may be borrowed
+    // and modified, but not consumed.
+    fn find<P>(&mut self, predicate: P) -> Option<Self::Item> where
+        // `FnMut` meaning any captured variable may at most be
+        // modified, not consumed. `&Self::Item` states it takes
+        // arguments to the closure by reference.
+        P: FnMut(&Self::Item) -> bool;
+}
